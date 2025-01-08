973d00fc644cde8fa556154db5f749c2b6a33709
Caden Feller <caden.feller@gmail.com>
2025-01-07 20:18:35 -0800

    updated RoboRIO documentation

--- RoboRIO/README.md
+++ RoboRIO/README.md
@@ -6,7 +6,16 @@ Currently, Java is the only supported implimentation at this time.
 ## Installation
 Unfortunatley, this program uses libraries that don't come pre-packaged with WPILib. This means that dependency installation is a little bit more involved than a `pip install`.
 
-Once you have your WPI project has been created, it already sets up a build system (Gradle) for you. Gradle manages dependencies before they get sent off to the robot. In order to install dependencies, we need to tell Gradle to inject the dependency into the jarfile that it compiles, before it sends that information off to the robot. Start by opening the `build.gradle` file in your favorite text editor. Find the `dependencies` section. On most projects, this starts around line 50. Once you've found this section, you'll need to add a couple of lines to the end of this section. On a new line within the curly brackets that contain the dependency section, add a new line with this code: `compile 'javax.websocket:javax.websocket-api:1.1'`, and another new line with thise code: `implementation 'org.json:json:20171018'`.
+Once you have your WPI project has been created, it already sets up a build system (Gradle) for you. Gradle manages dependencies before they get sent off to the robot. In order to install dependencies, we need to tell Gradle to inject the dependency into the jarfile that it compiles, before it sends that information off to the robot. Start by opening the `build.gradle` file in your favorite text editor. Find the `dependencies` section. On most projects, this starts around line 50. Once you've found this section, you'll need to add a couple of lines to the end of this section. On a new line within the curly brackets that contain the dependency section, add a couple new lines with this code: 
+
+```
+implementation 'org.java-websocket:Java-WebSocket:1.5.2'
+implementation 'com.google.code.gson:gson:2.8.9'
+implementation 'org.java-websocket:Java-WebSocket:1.5.2'
+implementation 'io.undertow:undertow-websockets-jsr:2.2.14.Final'
+implementation 'org.slf4j:slf4j-simple:1.7.32'
+
+```
 
 Voila! If done correctly, Gradle will include these dependencies in the jar file that runs on the RoboRIO. 
 
@@ -14,39 +23,30 @@ Voila! If done correctly, Gradle will include these dependencies in the jar file
 It is nessecary that you run a gradle build task with a stable internet connection before you deploy code to the robot, so that gradle can grab the dependencies from the web. If you aren't sure how to run a gradle build task, please look at an online tutorial or even the WPILib documentation, as the process is different between operating systems and IDEs. (Unless you're a CLI chad, where are my `./gradle build` brothers?).
 
 ## Code Implimentation
-We have provided you with several example files for your convenience in setting up Astrolabe. The first, `WebSocketClient.java` provides a framework for what a websocket implimentation should look like. This file contains a class that will represent the websocket client. It takes one manditory constructor, the server uri. Usage examples will come later. This implimentation uses an event system to handle sending and recieving messages. Then, you register a message handler or handlers to be triggered when you want to handle incoming messages. 
+We have provided you with `WebSocketClient.java`, which provides a framework for what a websocket implimentation should look like. This file contains a class that will represent the websocket client. Usage examples will come later. This implimentation uses an event system to handle sending and recieving messages.
 
-### Usage Examples
-
-*a more thourough example can be seen in `ExampleSubsystem.java`*
+### Usage Example
 
 creating a websocket instance - this should be done shortly after robot initialization
+
 ```java
 
-WebSocketClient websocket = new WebSocketClient("example.com:8080")
+String ip = "example.com:8000";
 
-websocket.addMessageHandler(new WebSocketClient.MessageHandler() {
+boolean failed = false;
+int i = 0;
+while(!failed) {
+    System.out.println("Trying to commect to: " + ip + ":" + (i + 50000));
+    // tries to connect to the given IP with a timeout of 1000ms
+    WebSocketClient newCam = new WebSocketClient(ip + ":" + (i + 50000), 1000);
+    newCam.setupConnection();
 
-    public void handleMessage(byte[] message) {
-        // handle incoming byte array
+    if(newCam.isConnected()) {
+        camClientList.add(newCam);
+        i++;
+    } else {
+        failed = true;
     }
-
-});
-
-
-```
-
-using the messaging system
-```java
-
-websocket.sendMessage("fp");
-
-// you will have to make and impliment this method however you like
-// we recommend that you decompile the json string into an object and assign those values to a class
-WebSocketResponse response = websocket.getLastResponse();
-
-distance = response.getDistance();
-angle = response.getAngle();
-// etc, etc ...
+}
 
 ```
--- RoboRIO/WebSocketClient.java
+++ RoboRIO/WebSocketClient.java
@@ -1,94 +1,259 @@
 package com.example.robotcode.websocket;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
+import java.net.http.WebSocket.Listener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
-import javax.websocket.ClientEndpoint;
-import javax.websocket.CloseReason;
-import javax.websocket.ContainerProvider;
-import javax.websocket.OnClose;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
-import javax.websocket.WebSocketContainer;
-
-@ClientEndpoint
 public class WebSocketClient {
+    private String ip = "ws://10.42.0.118:50000";
+    private WebSocket webSocket;
+    private double rotation;
+    private volatile String latestReply = "";
+    private CountDownLatch messageLatch = new CountDownLatch(1);
+    private final int TIMEOUT;
 
-    Session session;
-    private MessageHandler messageHandler;
+    public static class Color {
+        public double red;
+        public double green;
+        public double blue;
+        public double difference;
+        public double blur;
+    }
 
-    private WebSocketResponse lastResponse;
+    public static class Info {
+        public String cameraName;
+        public String identifier;
+        public double horizontalFocalLength;
+        public double verticalFocalLength;
+        public int height;
+        public int horizontalResolutionPixels;
+        public int verticalResolutionPixels;
+        public int processingScale;
+        public double tiltAngleRadians;
+        public double horizontalFieldOfViewRadians;
+        public double verticalFieldOfViewRadians;
+        public List<Color> colorList;
+        public int activeColor;
+        public String fullString;
+    }
 
-    public WebSocketClient(URI endpointURI) {
-        // tries to open a websocket connection, throws an error on faliure
-        // this behavior can be changed to not crash your program, but you might
-        // get errors later in the stack, so you'll have to waterproof that
-        try {
-            WebSocketContainer containter = ContainterProvider
-                .getWebsocketContianer();
-            containter.connectToServer(this, endpointURI);
+    public static class Apriltag {
+        public String tagId;
+        public double[] position;
+        public double[] orientation;
+        public double distance;
+        public double horizontalAngle;
+        public double verticalAngle;
+        public String fullString;
+    }
+
+    public CameraWebsocketClient() {
+        TIMEOUT = 5000;
+    }
+
+    public CameraWebsocketClient(String ip, int timeout) {
+        TIMEOUT = timeout;
+        this.ip = ip;
+    }
+
+    public boolean setupConnection() {
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            webSocket = client.newWebSocketBuilder()
+                    .buildAsync(URI.create(ip), new WebSocketListener(this))
+                    .join();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            return false;
         }
+        return isConnected();
     }
 
-
-    @OnOpen
-    public void onOpen(Session session) {
-        this.session = session;
+    public boolean isConnected() {
+        return webSocket != null && webSocket.isOutputClosed() == false && webSocket.isInputClosed() == false;
     }
 
-    @OnClose
-    public void onClose(Session session, CloseReason reason) {
-        this.session = null;
-    }
-
-    @OnMessage
-    public void onMessage(String message) {
-        if (this.messageHandler != null) {
-            this.messageHandler.handleMessage();
-        }
-    }
-
-    // registers a message handler to this object
-    public void addMessageHandler(MessageHandler messageHandler) {
-        this.messageHandler = messageHandler;
-    }
-
-    // simply sends a message to the server
     public void sendMessage(String message) {
-        this.userSession.getAsyncRemote().sendText(message);
+        webSocket.sendText(message, true);
     }
 
-    // user can impliment the message handler as they please
-    // the message handler methods should only be defined in this interface
-    // NO CODE for these should exist in this file. These methods should be implimented in a seperate file
-    public static interface MessageHandler {
-        // more messageHandler overrides can be added
-        public void handleMessage(byte[] message);
-        public void handleMessage(String message);
+    public void onMessage(String newMessage) {
+        System.out.println("Received message: " + newMessage);
+        this.latestReply = newMessage;
+        messageLatch.countDown();
     }
 
-    public class WebSocketResponse {
-        String type;
-        String error;
-        String distance; 
-        String rotation;
-        // etc etc, add all of the values you need
+    public String getMessage() {
+        return latestReply;
+    }
 
-        public WebSocketResponse(String type, String error) {
-            this.type = type;
-            this.error = error;
+    public String getLatestReply() {
+        try {
+            messageLatch.await(TIMEOUT, TimeUnit.MILLISECONDS); // Wait for up to 5 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        // etc etc, constructor overloads
+        return latestReply;
+    }
 
-        public String getType() {
-            return type;
+    public void clear() {
+        latestReply = "";
+      }
+
+    public JsonObject decodeJson(String jsonString) {
+        Gson gson = new Gson();
+        return gson.fromJson(jsonString, JsonObject.class);
+    }
+
+    public double getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(double rotation) {
+        this.rotation = rotation;
+    }
+
+    public Info getInfo() {
+        try {
+            sendMessage("info");
+            String newMessage = getLatestReply();
+            return getInfoFromString(newMessage);
+        } catch (Exception e) {
+            return new Info();
+        }
+    }
+
+    private Info getInfoFromString(String pMessage) {
+        if (pMessage == null){
+            return null;
+        }
+        try {
+            JsonObject json = decodeJson(pMessage);
+            Info info = new Info();
+            info.cameraName = json.get("cam_name").getAsString();
+            info.identifier = json.get("identifier").getAsString();
+            info.horizontalFocalLength = json.get("horizontal_focal_length").getAsDouble();
+            info.verticalFocalLength = json.get("vertical_focal_length").getAsDouble();
+            info.height = json.get("height").getAsInt();
+            info.horizontalResolutionPixels = json.get("horizontal_resolution_pixels").getAsInt();
+            info.verticalResolutionPixels = json.get("vertical_resolution_pixels").getAsInt();
+            info.processingScale = json.get("processing_scale").getAsInt();
+            info.tiltAngleRadians = json.get("tilt_angle_radians").getAsDouble();
+            info.horizontalFieldOfViewRadians = json.get("horizontal_field_of_view_radians").getAsDouble();
+            info.verticalFieldOfViewRadians = json.get("vertical_field_of_view_radians").getAsDouble();
+            info.activeColor = json.get("active_color").getAsInt();
+            info.colorList = new ArrayList<>();
+            for (var colorJson : json.getAsJsonArray("color_list")) {
+                Color color = new Color();
+                JsonObject colorObject = colorJson.getAsJsonObject();
+                color.red = colorObject.get("red").getAsDouble();
+                color.green = colorObject.get("green").getAsDouble();
+                color.blue = colorObject.get("blue").getAsDouble();
+                color.difference = colorObject.get("difference").getAsDouble();
+                color.blur = colorObject.get("blur").getAsDouble();
+                info.colorList.add(color);
+            }
+            info.fullString = pMessage;
+            return info;
+        } catch (Exception e) {
+            System.out.println("Error getting info");
+            e.printStackTrace();
+            return new Info();
+        }
+    }
+
+    public List<Apriltag> getApriltags() {
+        try {
+            sendMessage("fa");
+            String newMessage = getLatestReply();
+            return getApriltagsFromString(newMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (setupConnection()) {
+                sendMessage("fa");
+                String newMessage = getLatestReply();
+                return getApriltagsFromString(newMessage);
+            }
+            return new ArrayList<>();
+        }
+    }
+
+    private List<Apriltag> getApriltagsFromString(String pMessage) {
+        if (pMessage == null){
+            return null;
+        }
+        try {
+            JsonArray jsonArray = new Gson().fromJson(pMessage, JsonArray.class);
+            List<Apriltag> apriltags = new ArrayList<>();
+
+            if (jsonArray != null && jsonArray.size() > 0) {
+                for (var apriltagJson : jsonArray) {
+                    Apriltag apriltag = new Apriltag();
+                    JsonObject apriltagObject = apriltagJson.getAsJsonObject();
+                    apriltag.tagId = apriltagObject.get("tag_id").getAsString();
+                    apriltag.position = new double[]{
+                            apriltagObject.get("position").getAsJsonArray().get(0).getAsDouble(),
+                            apriltagObject.get("position").getAsJsonArray().get(1).getAsDouble(),
+                            apriltagObject.get("position").getAsJsonArray().get(2).getAsDouble()
+                    };
+                    apriltag.orientation = new double[]{
+                            apriltagObject.get("orientation").getAsJsonArray().get(0).getAsDouble(),
+                            apriltagObject.get("orientation").getAsJsonArray().get(1).getAsDouble(),
+                            apriltagObject.get("orientation").getAsJsonArray().get(2).getAsDouble()
+                    };
+                    apriltag.distance = apriltagObject.get("distance").getAsDouble();
+                    apriltag.horizontalAngle = apriltagObject.get("horizontal_angle").getAsDouble();
+                    apriltag.verticalAngle = apriltagObject.get("vertical_angle").getAsDouble();
+                    apriltags.add(apriltag);
+                }
+                apriltags.get(0).fullString = pMessage;
+            }
+
+            return apriltags;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    private static class WebSocketListener implements Listener {
+        private final CameraWebsocketClient client;
+
+        public WebSocketListener(CameraWebsocketClient client) {
+            this.client = client;
         }
 
-        // etc etc
-        
+        @Override
+        public void onOpen(WebSocket webSocket) {
+            System.out.println("WebSocket opened");
+            Listener.super.onOpen(webSocket);
+        }
 
-        // we do not recommend the use of setters, but nothing prevents you from doing so
+        @Override
+        public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+            client.onMessage(data.toString());
+            return Listener.super.onText(webSocket, data, last);
+        }
+
+        @Override
+        public void onError(WebSocket webSocket, Throwable error) {
+            error.printStackTrace();
+        }
+
+        @Override
+        public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+            System.out.println("WebSocket closed with status " + statusCode + " and reason " + reason);
+            return Listener.super.onClose(webSocket, statusCode, reason);
+        }
     }
 }
